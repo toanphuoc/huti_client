4344a926ae1d12f7e0c79100231f004d6859a8e7
= <=>
2016-10-07 14:49:23 +0700

    s
    edit page draw

--- js/draw/canvas.js
+++ js/draw/canvas.js
@@ -4,11 +4,6 @@ var canvas = new fabric.Canvas('canvas',{
 
 var widthScreen = $(window).width();
 canvas.setDimensions({width: 756, height: 302});
-// if(widthScreen >= 1200){
-// 	canvas.setDimensions({width: 756, height: 302});
-// }else if(widthScreen >= 992 && widthScreen < 1200){
-// 	canvas.setDimensions({width: 758, height: 250});
-// }
 
 var $canvas = $("#canvas");
 var canvasOffset = $canvas.offset();
@@ -16,8 +11,3 @@ var offsetX = canvasOffset.left;
 var offsetY = canvasOffset.top;
 
 var canvasView = new fabric.Canvas('canvas-view');
-// if(widthScreen >= 1200){
-// 	canvasView.setDimensions({width: 918 - 30, height: 300});
-// }else if(widthScreen >= 992 && widthScreen < 1200){
-// 	canvasView.setDimensions({width: 758 - 30, height: 250});
-// }
